48cfbd391a4a4bb367cea6cdae08c11cb534c743
freddygonzalez798 <145599055+freddygonzalez798@users.noreply.github.com>
2025-02-10 21:06:34 -0500

    se modifica el menu hamburguesa
    se modifica el menu hamburguesa

--- js/navbar.js
+++ js/navbar.js
@@ -9,4 +9,11 @@ document.querySelectorAll('nav ul li a').forEach(item => {
 function toggleMenu() {
     const menu = document.querySelector('nav ul');
     menu.classList.toggle('active');
+
+    // Cierra el menú al hacer clic en un elemento del menú
+    document.querySelectorAll('nav ul li a').forEach(item => {
+        item.addEventListener('click', () => {
+            menu.classList.remove('active'); // Cierra el menú
+        });
+    });
 }
